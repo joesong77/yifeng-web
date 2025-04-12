0b250abb0ee57d777d32a7bd2d117219a2fe0cb4
song rong yu <songgooe@gmail.com>
2025-04-12 20:45:19 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Home from "./pages/Home/Home"
 import AppBar from "./componets/AppBar/AppBar"
 import FooTer from "./componets/FooTer/FooTer"
+import { HashRouter } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
 
         <AppBar/>
+        <HashRouter>
         <main className='main-content'>
             <Home/>
         </main>
+        </HashRouter>
         <FooTer/>
         
     </div>
