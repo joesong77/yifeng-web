745512b03a694ad4bebb52a9efd32bd1db675dd0
joesong7 <72059440+joesong77@users.noreply.github.com>
2025-05-12 19:31:55 +0800

    刪除實績的router

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -71,6 +71,7 @@ const Home = () => {
                             initial={{ opacity: 0, y: 30 }}
                             animate={{ opacity: 1, y: 0 }}
                             transition={{ duration: 2 }}
+                            viewport={{ once: true, amount: 0.5 }} 
                         />
                     </div>
 
